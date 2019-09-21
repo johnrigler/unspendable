ed62545f27ff062efa1b9e532adbad9f387593bd
John Rigler <john@rigler.org>
2019-09-21 01:17:36 +0000

    Added seed reference and description

--- unspendable.py
+++ unspendable.py
@@ -7,6 +7,23 @@ import binascii
 dhash = lambda x: hashlib.sha256(hashlib.sha256(x).digest()).digest()
 b58_digits = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
 
+
+# Given the base-58 digits above the following numbers represent the seeds needed 
+# to begin the process for each character.  For example, bitcoin addresses start with
+# a 1 and the first seed is 0, dogecoin addresses can start with D and the number 30
+# below corresponds with the D in the field above.
+#
+# This removes the need to pass the seed as a parameter, because now it can just be referenced
+
+seed = [ '0','2','5','7','10','12','15','17','20','22','25',
+         '27','30','32','35','37','40','42','45','48','50',
+         '53','55','58','60','63','65','68','70','73','75',
+         '78','80','83','85','88','91','93','96','98','101',
+         '103','106','108','111','113','116','118','121',
+         '123','126','128','131','134','136','139','141','144' ]
+
+
+
 def base58_check_encode(b, version):
     d = version + b
     address = d + dhash(d)[:4]

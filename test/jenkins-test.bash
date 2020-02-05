4a993937b67a0f263c947aed78718f6c2542a375
John Rigler <john@rigler.org>
2020-02-05 21:33:43 +0000

    Need variables for Jenkinsfile

--- test/jenkins-test.bash
+++ test/jenkins-test.bash
@@ -11,11 +11,13 @@ else
 DIR=/root/unspendable
 fi
 
+pwd
+env
 CMD=$DIR/unspendable.py
 T="DD  DDDDDDDDD"
 for X in 1 2 3 4 5 6 7 8 9s 9t 9u 9v 9w 9x 9y 9z A B C D DC DCx E F G H J K mv
 do
-python3 $CMD $X "$T"
+$CMD $X "$T"
 done  } 
 
 unspendable_test

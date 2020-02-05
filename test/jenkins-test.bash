4c43f14059454ebd6529840695c521fdae30d034
John Rigler <john@rigler.org>
2020-02-05 21:56:06 +0000

    .

--- test/jenkins-test.bash
+++ test/jenkins-test.bash
@@ -7,7 +7,7 @@ unspendable_test() {
 T="DD  DDDDDDDDD"
 for X in 1 2 3 4 5 6 7 8 9s 9t 9u 9v 9w 9x 9y 9z A B C D DC DCx E F G H J K mv
 do
-unspendable.py $X "$T"
+./unspendable.py $X "$T"
 done  } 
 
 unspendable_test

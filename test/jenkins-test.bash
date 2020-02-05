a506afc6528ba2003632adc8fa5bc645bb06f269
John Rigler <john@rigler.org>
2020-02-05 21:43:05 +0000

    .

--- test/jenkins-test.bash
+++ test/jenkins-test.bash
@@ -4,20 +4,10 @@ unspendable_test() {
 
 : ..
 
-if [[ $CIRCLECI == 'true' ]]
-then
-DIR=/home/circleci/project
-else
-DIR=/root/unspendable
-fi
-
-pwd
-env
-CMD=$DIR/unspendable.py
 T="DD  DDDDDDDDD"
 for X in 1 2 3 4 5 6 7 8 9s 9t 9u 9v 9w 9x 9y 9z A B C D DC DCx E F G H J K mv
 do
-$CMD $X "$T"
+unspendable.py $X "$T"
 done  } 
 
 unspendable_test

fe168aa578bb0f52960cdd548891f25c7545b1ca
John Rigler <john@rigler.org>
2020-02-05 21:19:33 +0000

    .

--- test/jenkins-test.bash
+++ test/jenkins-test.bash
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 unspendable_test() {
 
 : ..
@@ -15,4 +17,9 @@ for X in 1 2 3 4 5 6 7 8 9s 9t 9u 9v 9w 9x 9y 9z A B C D DC DCx E F G H J K mv
 do
 python3 $CMD $X "$T"
 done  } 
+
+unspendable_test
+
+# This should always result in 020203
+
 unspendable_test | sum | grep 02023
